d8a3701bb208e06cc511b79d27dc06bf9e1deba1
Aris Husanu <abane94@gmail.com>
2026-08-09 16:10:40 -0400

    fix fronmatter tag formatting

--- lib/generate-markdown.ts
+++ lib/generate-markdown.ts
@@ -59,7 +59,7 @@ async function generateEntityFile(entityData: EntityData, vaultOutputFolder: str
         `name: ${entityData.name}`,
         `type: ${entityData.type}`,
         `description: ${entityData.description}`,
-        `tags: ${[...entityData.tags, entityData.type.toLowerCase()].map(t => `  - "${t}"`).join(', ')}`,
+        `tags:\n${[...entityData.tags, entityData.type.toLowerCase()].map(t => `  - "${t}"`).join(', ')}`,
         `aliases: ${entityData.aliases.map(a => `  - "${a}"`).join(', ')}`,
         `createdAt: ${entityData.createdAt}`,
         `updatedAt: ${entityData.updatedAt}`,
